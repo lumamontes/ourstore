e39bb9273c51e69d14aa4e5d9bd6fd146b5c0368
Luma Montes <lumagoesmontes@gmail.com>
2021-01-02 07:20:42 -0300

    criado logica para slide de fotos

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let search = document.getElementById('searchButton') // input
+let searchButton = document.getElementById('searchButton') // input
 let itensList = document.getElementById('itensList') // ul
 let itens = document.getElementsByTagName('li') // li
 
 function searchItens () {
-    filter = search.value.toUpperCase();
-    console.log(filter)
+    filter = searchButton.value.toUpperCase();
     for (item of itens) {
     txtValue = item.textContent || item.innerText
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
@@ -14,5 +13,32 @@ function searchItens () {
      }}
     
 }
+searchButton.addEventListener('keyup', searchItens)
 
-search.addEventListener('keyup', searchItens)
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName("slideImage");
+  console.log(slides.length)
+  var dots = document.getElementsByClassName("nextButton");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for( slide of slides) {
+    slide.style.display ="none"
+  }
+  for(dot of dots) {
+    dot.className = dot.className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
